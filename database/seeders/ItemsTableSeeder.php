498df726fab063ac40d1280ac1618ab6e42a5cc9
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-02 15:42:54 +0200

    [TEST]

---
+++ database/seeders/ItemsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+{
+    if (($handle = fopen(storage_path('app/csv/items.csv'), 'r')) !== false) {
+        while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+            DB::table('items')->insert([
+                'name' => $data[0],
+                'slug' => $data[1],
+                'type' => $data[2],
+                'category' => $data[3],
+                'weight' => $data[4],
+                'cost' => $data[5],
+                'damege_dice' => $data[6],
+
+            ]);
+        }
+        fclose($handle);
+    }
+}
+}
